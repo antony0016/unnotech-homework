0c4463498d579622594599f5850fad45449067e3
SekiXu <matsuno.seki@gmail.com>
2026-04-15 02:09:01 +0800

    feat: create FocusNews model

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FocusNews
+
+
+@admin.register(FocusNews)
+class FocusNewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'published_at', 'created_at')
+    search_fields = ('title', 'author')
+    readonly_fields = ('created_at',)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class FocusNews(models.Model):
+    title = models.CharField(max_length=256)
+    url = models.URLField(unique=True)
+    image_url = models.URLField()
+    image_caption = models.CharField(max_length=256, blank=True)
+    content = models.TextField(blank=True)
+    author = models.CharField(max_length=64, blank=True)
+    published_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.title}/{self.author}"
